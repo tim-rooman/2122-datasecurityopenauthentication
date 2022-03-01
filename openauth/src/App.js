d29a4690b20ab6d69abaa0eb33a691c8bfb84f15
timrooman <tim.rooman@student.odisee.be>
2022-03-01 14:07:19 +0100

    CSS changes.

--- openauth/src/App.js
+++ openauth/src/App.js
@@ -16,8 +16,8 @@ function App() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(authentication, provider)
       .then((re) => {
-        const credential = GoogleAuthProvider.credentialFromResult(re);
-        const token = credential.accessToken;
+        // const credential = GoogleAuthProvider.credentialFromResult(re);
+        // const token = credential.accessToken;
         setUSer(re.user);
         const user = re.user;
         console.log(user);
@@ -41,18 +41,30 @@ function App() {
     <div className="App">
       <div className="header">
         <span>Home</span>
-        {user ? <img src={user.photoURL} /> : null}
+        {user ? <img src={user.photoURL} alt="User profile" /> : null}
       </div>
-      {user ? <span>Hi {user.displayName} </span> : <span>Please log in</span>}
       {user ? (
-        <div>
-          {user.email} <img src={user.photoURL} />
+        <h1>Greetings, {user.displayName} </h1>
+      ) : (
+        <span>Please log in</span>
+      )}
+      {user ? (
+        <div className="flexContainer">
+          <div className="infoBox">
+            <img src={user.photoURL} alt="User profile" />
+            <p>Your displayname is: {user.displayName}</p>
+            <p>Your email is: {user.email}</p>
+          </div>
         </div>
       ) : null}
       {user ? (
-        <button onClick={signOutFromGoogle}>Log out</button>
+        <button onClick={signOutFromGoogle} className="login-with-google-btn">
+          Log out
+        </button>
       ) : (
-        <button onClick={signInWithGoogle}>Log in with Google</button>
+        <button onClick={signInWithGoogle} className="login-with-google-btn">
+          Log in with Google
+        </button>
       )}
     </div>
   );
