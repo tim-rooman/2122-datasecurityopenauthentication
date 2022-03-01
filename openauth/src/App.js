b6d2cddbcc8102b7588f2e48ca2b1f4d9a3cc083
timrooman <tim.rooman@student.odisee.be>
2022-03-01 13:07:28 +0100

    Adding basic css and log out feature.

--- openauth/src/App.js
+++ openauth/src/App.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import { authentication } from "./firebase-config";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
 
 function App() {
   const [user, setUSer] = useState(null);
@@ -22,15 +27,33 @@ function App() {
       });
   };
 
+  const signOutFromGoogle = () => {
+    signOut(auth)
+      .then(() => {
+        setUSer(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
+      <div className="header">
+        <span>Home</span>
+        {user ? <img src={user.photoURL} /> : null}
+      </div>
       {user ? <span>Hi {user.displayName} </span> : <span>Please log in</span>}
       {user ? (
         <div>
           {user.email} <img src={user.photoURL} />
         </div>
       ) : null}
-      <button onClick={signInWithGoogle}>Log in with Google</button>
+      {user ? (
+        <button onClick={signOutFromGoogle}>Log out</button>
+      ) : (
+        <button onClick={signInWithGoogle}>Log in with Google</button>
+      )}
     </div>
   );
 }
