4bee0e9ef8ccaa9d18021617d76c04c2a46e2077
timrooman <tim.rooman@student.odisee.be>
2022-03-01 11:02:23 +0100

    Adding Firebase config.

---
+++ openauth/src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDS74fCLBtgnYv_srgUrn2YJP0XXlZP5vo",
+  authDomain: "openauthproofofconcept.firebaseapp.com",
+  projectId: "openauthproofofconcept",
+  storageBucket: "openauthproofofconcept.appspot.com",
+  messagingSenderId: "936945897277",
+  appId: "1:936945897277:web:1f78265e3818bf5a96ff54",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
